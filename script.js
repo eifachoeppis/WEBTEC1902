db265b9287195195dc5940fa47fa93a278dc04db
unknown <lukas.huber@stud.hslu.ch>
2019-11-30 17:25:36 +0100

    added image

--- script.js
+++ script.js
@@ -3,54 +3,68 @@
     const dateTime = new Date();
     document.cookie = "last_hit=" + dateTime.toLocaleDateString() + " - " + dateTime.toLocaleTimeString();
 
-    debugger;
+    document.addEventListener("DOMContentLoaded", function(){
+        const ball = document.getElementById("ball");
+        const context = ball.getContext("2d");
 
-    const ball = document.getElementById("ball");
-    const context = ball.getContext("2d");
+        const timeout = 5;
+        const radius = 10;
+        let x = radius;
+        let y = radius;
+        let deltaX = 2;
+        let deltaY = 2;
+        let interval = start();
+        let isRunning = true;
 
-    const timeout = 5;
-    const radius = 10;
-    let x = radius;
-    let y = radius;
-    let deltaX = 2;
-    let deltaY = 2;
-    let interval = start();
-    let isRunning = true;
+        ball.addEventListener("click", function(e){
+            if (isRunning){
+                clearInterval(interval);
+                isRunning = false;
+            }
+            else{
+                interval = start();
+                isRunning = true;
+            }
+        });
 
-
-    ball.addEventListener("click", function(e){
-        if (isRunning){
-            clearInterval(interval);
-            isRunning = false;
+        function start() {
+            return setInterval(drawCircle, timeout);
         }
-        else{
-            interval = start();
-            isRunning = true;
+
+        function drawCircle() {
+            context.clearRect(0, 0, ball.width, ball.height);
+            context.beginPath();
+            context.fillStyle = "#FFFF00";
+            context.arc(x, y, radius, 0, 2 * Math.PI);
+            context.closePath();
+            context.fill();
+
+            if (x < radius || x > ball.width - radius){
+                deltaX = -deltaX;
+            }
+            if (y < radius || y > ball.height - radius){
+                deltaY = -deltaY;
+            }
+
+            x += deltaX;
+            y += deltaY;
         }
+
+        const actionText = document.getElementById("actionText");
+        actionText.addEventListener("click", function (e) {
+            const imageLink = "content/Skierffe.jpg";
+            fetch(imageLink)
+                .then(function (response) {
+                    console.log(response.blob());
+                    return response.blob();
+                })
+                .then(function (blob) {
+                    const imageUrl = URL.createObjectURL(blob);
+                    const img = document.createElement("img");
+                    img.src = imageUrl;
+                    e.target.parentElement.appendChild(img);
+                    actionText.remove();
+                });
+        });
     });
-
-    function start() {
-        return setInterval(function () {
-            drawCircle(ball);
-        }, timeout);
-    }
-
-    function drawCircle(ball) {
-        context.clearRect(0, 0, ball.width, ball.height);
-        context.beginPath();
-        context.fillStyle = "#FFFF00";
-        context.arc(x, y, radius, 0, 2 * Math.PI);
-        context.closePath();
-        context.fill();
-
-        if (x < radius || x > ball.width - radius){
-            deltaX = -deltaX;
-        }
-        if (y < radius || y > ball.height - radius){
-            deltaY = -deltaY;
-        }
-
-        x += deltaX;
-        y += deltaY;
-    }
 })();
